60366b040225318e2aaec1364e653624efa28c7c
Nazeef ul Hassan Qureshi <nhqureshi7717@gmail.com>
2026-06-03 13:31:53 +0500

    Added modulus operation

--- Calculator.java
+++ Calculator.java
@@ -6,5 +6,6 @@ public class Calculator {
         System.out.println("Subtraction = " + (a - b));
         System.out.println("Multiplication = " + (a * b));
         System.out.println("Division = " + (a / b));
+        System.out.println("Modulus = " + (a % b)); // New line added
     }
 }
